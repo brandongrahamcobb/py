118f651cb16ab925f4c41eca4594588b8bff1f78
brandongrahamcobb <brandongrahamcobb@icloud.com>
2024-07-07 08:52:23 -0400

    deprecated .env procedure

--- Listener.py
+++ Listener.py
@@ -2,6 +2,7 @@
 import discord
 import os
 from discord.ext import commands
+from subprocess import run
 
 class Listener(commands.Cog):
 
@@ -9,18 +10,11 @@ class Listener(commands.Cog):
         self.bot = bot
         self_last_member = None
 
-    @commands.Cog.listener()
-    @commands.is_owner()
-    async def on_message(self, message: discord.Message) -> None:
-        if message.author.bot:
-            return
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        if not os.path.exists('/home/spawd/Documents/txt/version.txt'):
-             with open('/home/spawd/Documents/txt/version.txt', 'w') as f:
+    def version():
+        if not os.path.exists('../txt/version.txt'):
+             with open('../txt/version.txt', 'w') as f:
                  f.write('1.0.0')
-        with open('/home/spawd/Documents/txt/version.txt', 'r') as f:
+        with open('../txt/version.txt', 'r') as f:
              version = f.read().strip()
         major, minor, patch = map(int, version.split('.'))
         patch += 1
@@ -31,8 +25,19 @@ class Listener(commands.Cog):
             minor = 0
             major += 1
         version = f"{major}.{minor}.{patch}"
-        with open('/home/spawd/Documents/txt/version.txt', 'w') as f:
+        with open('../txt/version.txt', 'w') as f:
             f.write(version)
+        return version
+
+    @commands.Cog.listener()
+    @commands.is_owner()
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.bot:
+            return
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        version = Listener.version()
         print('Logged on as', self.bot.user)
         print(f'Bot ID: {self.bot.user.id}')
         print(f'Bot Version: {version}')
