82eb78af78658ce5e549266d95996275d5b766a5
brandongrahamcobb <brandongrahamcobb@icloud.com>
2024-07-15 11:33:20 -0400

    added colour command for bot

--- Chemistry.py
+++ Chemistry.py
@@ -56,7 +56,7 @@ class Chemistry(commands.Cog):
         draw.text(((width + (width - draw.textlength(label_2, font=font)) / 2) + 4, 14), label_2, fill="gray", font=font)
         draw.text(((width - draw.textlength(label_1, font=font)) / 2, 10), label_1, fill="black", font=font)
         draw.text((width + (width - draw.textlength(label_2, font=font)) / 2, 10), label_2, fill="black", font=font)
-        text = f"Tanimoto Similarity: {similarity:.2f}"
+        text = f"Tanimoto Similarity: {similarity:.5f}"
         draw.text((((new_image.width - draw.textlength(text, font=font)) / 2) + 4, height - 56), text, fill="gray", font=font)
         draw.text(((new_image.width - draw.textlength(text, font=font)) / 2, height - 60), text, fill="black", font=font)
         output_buffer = io.BytesIO()
