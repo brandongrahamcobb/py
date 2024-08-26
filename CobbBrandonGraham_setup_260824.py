ec9ab7abd41b47394b88aee4475f38643e00e032
brandongrahamcobb <brandongrahamcobb@icloud.com>
2024-08-26 09:30:34 -0400

    current source

---
+++ CobbBrandonGraham_setup_260824.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lucy',
+    version='260824',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'biopython',
+        'bs4',
+        'discord.py',
+        'emoji',
+        'google-api-python-client',
+        'googletrans==4.0.0-rc1',
+        'pip',
+        'pubchempy',
+        'pillow',
+        'requests',
+        'rdkit',
+        'selenium',
+        'webdriver_manager',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lucy=bot.main:run',
+        ],
+    },
+    data_files=[
+        ('/usr/share/lucy/resources', [
+            'resources/config.json',
+            'resources/users.json',
+        ]),
+        # Add other resource files or directories as needed
+    ],
+    zip_safe=False,
+)
