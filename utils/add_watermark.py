520630cd4ea7ae1ac5d56705bbafff85f158ea3c
Brandon Graham Cobb <brandongrahamcobb@icloud.com>
2024-12-17 08:31:27 -0500

    method

---
+++ utils/add_watermark.py
@@ -0,0 +1,33 @@
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+
+def add_watermark(image: BytesIO, watermark_text: str = 'Discord') -> BytesIO:
+    RGB_image = Image.open(image)
+    RGBA_image = RGB_image.convert('RGBA')
+    draw = ImageDraw.Draw(RGBA_image)
+    width, height = RGBA_image.size
+    diagonal = math.sqrt(width**2 + height**2)
+    font_size = int(diagonal / 15)
+    try:
+        font = ImageFont.truetype('Roboto-Regular.ttf', font_size)
+    except IOError:
+        font = ImageFont.load_default()
+    while True:
+        bbox = draw.textbbox((0, 0), watermark_text, font=font)
+        text_width = bbox[2] - bbox[0]
+        if text_width <= 512 or font_size <= 1:
+            break
+        font_size -= 1
+        font = ImageFont.truetype('Roboto-Regular.ttf', font_size)
+    text_height = bbox[3] - bbox[1]
+    text_x = (width - text_width) / 2
+    text_y = height - (2 * text_height)
+    watermark_image = Image.new('RGBA', RGBA_image.size, (0, 0, 0, 0))
+    watermark_draw = ImageDraw.Draw(watermark_image)
+    watermark_draw.text((text_x, text_y), watermark_text, font=font, fill=(255, 255, 255, 64))
+    mask = watermark_image.split()[3]
+    RGBA_image.paste(watermark_image, (0, 0), mask)
+    output = BytesIO()
+    RGBA_image.save(output, format= 'PNG')
+    output.seek(0)
+    return output
