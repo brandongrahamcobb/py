e154f2d8c50b1dd5cabd2bf02cf9db9140c29dab
Brandon Graham Cobb <brandongrahamcobb@icloud.com>
2024-12-26 16:22:51 -0500

    training.jsonl

--- bots/lucy/utils/average_score.py
+++ bots/lucy/utils/average_score.py
@@ -1,3 +1,20 @@
+''' average_score.py  The purpose of this program is present the average score of the positive responses from cd ../.
+    Copyright (C) 2024  github.com/brandongrahamcobb
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import asyncio
 import json
 import os
@@ -9,8 +26,12 @@ async def main():
     path_training = os.path.join(home, 'py', 'bots', 'lucy', 'training.jsonl')
 
     with open(path_training, 'r') as file:
-        taining_data = json.load(file)
-        print(training_data)
+        training_data = json.load(file)
+        sum = 0
+        for arg in training_data:
+            sum += arg['messages'][1]['content']['sentiment_score']
+        average = sum / len(training_data)
+        print(average)
 
     file.close()
 
